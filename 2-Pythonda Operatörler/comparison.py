5acaddb054bb808ad98108ff7c3ab810b4e0883c
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2021-02-23 20:18:13 +0300

    Update comparison.py

--- 2-Pythonda Operatörler/comparison.py
+++ 2-Pythonda Operatörler/comparison.py
@@ -1,16 +1,16 @@
 # username, password => database
 
-# 'sadikturan' , '123456'
+# 'ebubekirdgn' , '123456'
 
 a, b, c, d = 5, 5, 10, 4
 
 password = '1234'
-username = 'sadikturan'
+username = 'ebubekirdgn'
 
 result = (a == b) # True
 result = (a == c) # False
-result = ('sdktrn'== username)
-result = ('sadikturan'== username)
+result = ('dgn'== username)
+result = ('ebubekirdgn'== username)
 result = (a != b)
 result = (a != c)
 result = (a > c)
