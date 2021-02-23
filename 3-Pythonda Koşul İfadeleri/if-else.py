77ff1193fc8fb257d1b249f0cdd2a92d20a0bd39
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2021-02-23 20:32:39 +0300

    Koşul İfadeleri
    Koşul İfadeleri

---
+++ 3-Pythonda Koşul İfadeleri/if-else.py
@@ -0,0 +1,12 @@
+username = 'ebubekirdgn'
+password = '1234'
+
+if (username == 'ebubekirdgn'):
+
+    if (password == '12345'):
+        print('Hoş geldiniz')
+    else:
+        print('parola yanlış')
+        
+else:
+    print('username yanlış')
