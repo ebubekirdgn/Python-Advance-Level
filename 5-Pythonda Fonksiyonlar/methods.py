e7c757d1bffe0b18f3818544c434a59892397cea
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2021-02-24 19:37:20 +0300

    Functions
    Functions

---
+++ 5-Pythonda Fonksiyonlar/methods.py
@@ -0,0 +1,19 @@
+
+# method
+
+list = [1,2,3]
+
+list.append(4)
+list.pop()
+
+print(type(list))
+print(list)
+
+myString = 'Hello Ebubekir'
+
+print(myString.upper())
+
+
+print(type(myString))
+
+# fonksiyon
