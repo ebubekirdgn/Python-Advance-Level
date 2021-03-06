4ffbc794d2d2a02221672b2eeb9bf72c7b745442
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2021-03-06 17:09:21 +0300

    Add mod

---
+++ 7 - Modules/mod.py
@@ -0,0 +1,25 @@
+'''
+   modül hakkında bilgilendirme 
+'''
+
+print('modül eklendi')
+
+number = 10
+
+numbers = [1,2,3,5]
+
+person = {
+    "name": "ebubekir",
+    "job":"engineer",
+    "city":"istanbul"
+}
+
+def func(x):
+    '''
+        fonksiyon hakkında bilgilendirme
+    '''
+    print(f'x: {x}')
+
+class Person:
+    def speak(self):
+        print("I'm speaking")
