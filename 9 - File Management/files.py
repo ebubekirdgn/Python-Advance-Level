661aa993faa9a1be589db19d77274d86b279a239
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-03-20 13:47:53 +0300

    update

--- 9 - File Management/files.py
+++ 9 - File Management/files.py
@@ -23,10 +23,10 @@ file.close()
 
 
 # "a": (Append) ekleme. Dosya konumda yoksa oluşturur. Verinin üzerine ekleme yapar
-# file = open("newfile.txt","a",encoding='utf-8')
-# file.write("\nEbubekir Dogan")
-# file.write("Emir Asaf Dogan\n")
-# file.close()
+file = open("newfile.txt","a",encoding='utf-8')
+file.write("\nEbubekir Dogan")
+file.write("Emir Asaf Dogan\n")
+file.close()
 
 # "x": (Create) oluşturma. Dosya zaten varsa hata verir.
 # file = open("newfile2.txt","x",encoding='utf-8')
