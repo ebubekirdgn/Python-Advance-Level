f57377ba0dc980a56381a12da5d49f3f31681c1e
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-03 14:36:14 +0300

    Selenium

---
+++ 14 - Selenium/install.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+driver = webdriver.Chrome('C:/Users\Ebubekir Dogan/Documents/GitHub/Python-Advance-Level/14 - Selenium/chromedriver') 
+
+url:"https://ebubekirdgnn.blogspot.com/"
+
+driver.get(url)
