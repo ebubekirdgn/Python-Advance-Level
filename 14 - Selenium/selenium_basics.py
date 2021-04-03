afd34a4c780d33949486a99690a67b00ce22cc58
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-03 16:41:20 +0300

    basic_selenium

--- 14 - Selenium/selenium_basics.py
+++ 14 - Selenium/selenium_basics.py
@@ -1,16 +1,30 @@
-
 from selenium import webdriver
 import time
 
 driver = webdriver.Chrome('C:/Users/Ebubekir Dogan/Documents/GitHub/Python-Advance-Level/14 - Selenium/chromedriver') 
 
-url:"https://github.com"
-driver.get(url)
 
+url = "http://github.com"
+driver.get(url)
 
 time.sleep(2)
 driver.maximize_window()
+driver.save_screenshot("github.com-homepage.png")
+
+url = "http://github.com/ebubekirdgn"
+driver.get(url)
+
 print(driver.title)
 
+if "ebubekirdgn" in driver.title:
+    driver.save_screenshot("github-ebubekirdgn.png")
+
 time.sleep(2)
+
+driver.back()
+# driver.forward()
+
+time.sleep(2)
+
 driver.close()
+
