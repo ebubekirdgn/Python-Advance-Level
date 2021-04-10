aab489a5b079645e526b66439a6d6d2f2c47aa28
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-10 19:17:32 +0300

    close #6 tweetleri cekip dosyaya kaydetme

--- 14 - Selenium/twitter.py
+++ 14 - Selenium/twitter.py
@@ -29,20 +29,21 @@ class Twitter:
     def search(self , hashtag):
         searchInput = self.browser.find_element_by_xpath("//*[@id='react-root']/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div[2]/input")
         searchInput.send_keys(hashtag)
+        time.sleep(2)
         searchInput.send_keys(Keys.ENTER)
         time.sleep(2)
 
+        results = []
+
         list = self.browser.find_elements_by_xpath("//div[@data-testid='tweet']/div[2]/div[2]")
-        for item in list:
-            print(len(list))
-            print("*************")
-            print(item.text)
-        
+        time.sleep(2)
+        print("count: "+ str(len(list)))
 
-       
-        last_height= self.browser.execute_script("return document.documentElement.scrollHeight")
-        loopCounter=0
+        for i in list:
+            results.append(i.text)
 
+        loopCounter = 0
+        last_height = self.browser.execute_script("return document.documentElement.scrollHeight")
         while True:
             if loopCounter > 5:
                 break
@@ -53,12 +54,36 @@ class Twitter:
                 break
             last_height = new_height
             loopCounter+=1
-        
-     
-        # time.sleep(2)
-        # print("count: "+ str(len(list)))
+
+            list = self.browser.find_elements_by_xpath("//div[@data-testid='tweet']/div[2]/div[2]")
+            time.sleep(2)
+            print("count: "+ str(len(list)))
+
+            for i in list:
+                results.append(i.text)
+
+        count = 1
+        with open("tweets.txt","w",encoding="UTF-8") as file:
+            for item in results:
+                file.write(f"{count}-{item}\n")
+                count+=1
+
+
+        # count = 1
+        # for item in results:            
+        #     print(f"{count}-{item}")
+        #     count+=1
+        #     print("***********")
+
+
 
 twitter = Twitter(username,password)
 # login
 twitter.singIn()
 twitter.search("python")
+
+
+
+    
+
+
