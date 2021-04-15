b350216a574c6fea18a9e64ed3b43d22b9ab3b14
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-15 19:16:03 +0300

    İnstagram

--- 14 - Selenium/instagram.py
+++ 14 - Selenium/instagram.py
@@ -7,7 +7,7 @@ class Instagram:
     def __init__(self,username,password):
         self.browserProfile = webdriver.ChromeOptions()
         self.browserProfile.add_experimental_option('prefs', {'intl.accept_languages':'en,en_US'})
-        self.browser = webdriver.Chrome('chromedriver.exe', chrome_options=self.browserProfile)
+        self.browser = webdriver.Chrome('C:/Users/Ebubekir Dogan/Documents/GitHub/Python-Advance-Level/14 - Selenium/chromedriver', chrome_options=self.browserProfile)
         self.username = username
         self.password = password
 
@@ -15,8 +15,8 @@ class Instagram:
         self.browser.get("https://www.instagram.com/accounts/login/")
         time.sleep(3)
         
-        usernameInput = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input")
-        passwordInput = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input")
+        usernameInput = self.browser.find_element_by_xpath("//*[@id='loginForm']/div/div[1]/div/label/input")
+        passwordInput = self.browser.find_element_by_xpath("//*[@id='loginForm']/div/div[2]/div/label/input")
 
         usernameInput.send_keys(self.username)
         passwordInput.send_keys(self.password)
