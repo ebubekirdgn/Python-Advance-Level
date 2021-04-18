1412d5cc863b6427316fdec257686b16f063262b
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-18 17:40:38 +0300

    OpenCv

---
+++ OpenCv-Computer Vision-2021/HandTrackingProject/FingerCountingProject.py
@@ -0,0 +1,80 @@
+import time
+import os
+from cv2 import cv2
+import HandTrackingModule as htm 
+wCam,hCam = 648,480
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # pc kamerasının acılmasını saglar
+cap.set(3,wCam)
+cap.set(4,hCam)
+
+folderPath = "FingerImages"
+myList = os.listdir(folderPath) #resimlerin liste halinde alınmasını saglar.
+#print(myList)
+overlayList= []
+
+
+#klasördeki resimleri okuyoruz cv2 sayesinde
+for imPath in myList:
+     image= cv2.imread(f'{folderPath}/{imPath}')
+     # image degiskeninin icinde resimler bu sekilde tutuluyor.
+     # 1.Jpg ve diğerleri içinde aynı sekilde
+     #[255 255 255]
+     #[255 255 255]
+     #[255 255 255]
+     
+     
+     #print(f'{folderPath}/{imPath}'))  
+     overlayList.append(image) # 6 adet resimimizi bu sekilde aldık.
+
+#print(len(overlayList))
+
+
+pTime = 0
+
+detector = htm.handDetector(detectionCon = 0.75)
+
+tipIds = [4,8,12,16,20]
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+ 
+    if len(lmList) != 0:
+        fingers = []
+ 
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+ 
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+ 
+        # print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers) #burada hangi parmagı kaldırdıgımızı buluyoruz
+ 
+        h, w, c = overlayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overlayList[totalFingers - 1]
+ 
+        cv2.rectangle(img, (20, 225), (0, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                    10, (255, 0, 0), 25)
+ 
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+ 
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+ 
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
