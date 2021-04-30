98e7d88c427c7cb9c9201db226ff115bcfa063cb
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-30 23:10:13 +0300

    add second migrations

--- 16- Django/catalog/movies/admin.py
+++ 16- Django/catalog/movies/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Movie
 # Register your models here.
 
-admin.site.register(Movie)
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id','name','description')
+    list_display_links = ('id','name')
+    list_filter =('created_date',)
+#register your models here.add()
+
+admin.site.register(Movie,MovieAdmin)
--- 16- Django/catalog/movies/models.py
+++ 16- Django/catalog/movies/models.py
@@ -5,8 +5,10 @@ from django.db import models
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.CharField(max_length=50)
-    created_date = models.DateTimeField(auto_now_add=True)
-    
+    name = models.CharField(max_length=100,verbose_name="Film Adı")
+    description = models.TextField(verbose_name="Açıklaması")
+    image = models.CharField(max_length=50,verbose_name="Film Resmi")
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name="Eklenme Tarihi")
+    isPublished = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
