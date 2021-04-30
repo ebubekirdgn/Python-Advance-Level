d9ceea990e7cb49f982035e2eb9f80fbb4e91753
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-01 00:30:02 +0300

    user app

--- 16- Django/catalog/catalog/settings.py
+++ 16- Django/catalog/catalog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages',
     'movies',
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- 16- Django/catalog/catalog/urls.py
+++ 16- Django/catalog/catalog/urls.py
@@ -21,5 +21,6 @@ from django.urls import path,include
 urlpatterns = [
     path('', include('pages.urls')),
     path('movies/', include('movies.urls')),
+    path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
