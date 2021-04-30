eadbb8cb5ea5ad8fa375f441190377b9d29f1549
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-05-01 00:27:22 +0300

    movie detail

--- 16- Django/catalog/movies/views.py
+++ 16- Django/catalog/movies/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Movie
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
-
 def index(request):
     movies = Movie.objects.all()
 
@@ -11,9 +13,15 @@ def index(request):
     }
     return render(request, 'movies/list.html', context)
 
-def detail(request):
-    return render(request, 'movies/detail.html')
+
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie, pk = movie_id)
+    context = {
+        'movie': movie
+    }
+
+    return render(request, 'movies/detail.html', context)
+
 
 def search(request):
     return render(request, 'movies/search.html')
-
