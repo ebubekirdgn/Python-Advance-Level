79a13566abf948509a1d03e521fb78cc6f5c0001
ebubekirdgn <ebubekir.dogan@bil.omu.edu.tr>
2021-04-30 17:34:39 +0300

    admin panel

--- DJANGO/catalog/catalog/settings.py
+++ DJANGO/catalog/catalog/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'tr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EUROPE/ISTANBUL'
 
 USE_I18N = True
 
