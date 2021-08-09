f642f5cd6dcc01296a7026fa4a01f2a0384d9842
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-08-09 10:48:53 +0300

    update os

--- 18 - Advanced Python Modules/_os.py
+++ 18 - Advanced Python Modules/_os.py
@@ -38,15 +38,15 @@ result = os.name
 # path
 
 result = os.path.abspath("_os.py")
-result = os.path.dirname("C:/python/advanced-modules/_os.py")
-result = os.path.dirname(os.path.abspath("_os.py"))
-result = os.path.exists("C:/python/advanced-modules/_os1.py")
-result = os.path.exists("C:/python/advanced-modules")
-result = os.path.isdir("C:/python/advanced-modules")
-result = os.path.isfile("C:/python/advanced-modules/_os.py")
-result = os.path.join("C:\\","deneme","deneme1")
-result = os.path.split("C:\\deneme")
-result = os.path.splitext("_os.py")
+result = os.path.dirname("C:/python/advanced-modules/_os.py") 
+result = os.path.dirname(os.path.abspath("_os.py")) # sadece dosya ismi bilinen dosyanın adresini bulma
+result = os.path.exists("C:/python/advanced-modules/_os1.py") #dizin yada dosya ilgili konumda var mı yok mu 
+result = os.path.exists("C:/python/advanced-modules") #dizin sorguladık.
+result = os.path.isdir("C:/python/advanced-modules") #ulastıgımız alan klasor mu 
+result = os.path.isfile("C:/python/advanced-modules/_os.py") # ulastıgımız alan dosya mı 
+result = os.path.join("C:\\","deneme","deneme1")  #verdigimiz parcaları birlestiririz 
+result = os.path.split("C:\\deneme") # dosya yolunu parcalar
+result = os.path.splitext("_os.py") # dosya ismi ile uzantisini ayirir.
 # result = result[0]
 result = result[1]
 
