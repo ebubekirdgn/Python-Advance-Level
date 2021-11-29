a05d44e265eb4d63a412e9b7a3838eddd023d4fd
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2021-11-30 02:09:24 +0300

    update

--- 16_Pandas/Covid19 Turkey/covid19.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-confirmed_df = pd.read_csv("worldometer_coronavirus_daily_data.csv")
-confirmed_df = confirmed_df.groupby(["country"]).aggregate(np.sum).T
-confirmed_df.index.name ="date"
-confirmed_df.
-
-print(confirmed_df.head())
