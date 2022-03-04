02ccb730768069836b14bf375de06d43cebc4533
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-03-04 11:40:31 +0300

    Update usom.py

--- 23-Cyber_Securit_Module/USOM/usom.py
+++ 23-Cyber_Securit_Module/USOM/usom.py
@@ -1,6 +1,5 @@
 import requests
 
-
 response = requests.get("https://www.usom.gov.tr/url-list.txt",verify=False)
 file = open("usom.txt","w") # zararlı olan alan adları alınıp usom.txt dosyasına kaydedildi.
 file.write(str(response.content))
